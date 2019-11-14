61a3b1985f5e12444ca38a010d55f6037b7f1e19
Damian Serwin <damian.abel.serwin@gmail.com>
2019-11-14 19:42:48 +0100

    Cleanups database migrations

--- cyclone-daemon/src/main/resources/db/migration/V1__Create_parcels_metadata_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE parcels_metadata
-(
-  id    text NOT NULL,
-  value jsonb
-);
-
-ALTER TABLE ONLY parcels_metadata ADD CONSTRAINT parcels_metadata_pkey PRIMARY KEY (id);
--- cyclone-daemon/src/main/resources/db/migration/V3__Create_test_run_status_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE test_run_status
-(
-  test_run_id character varying NOT NULL,
-  node_identifier text NOT NULL,
-  state text,
-  configuration jsonb
-);
-
-ALTER TABLE ONLY test_run_status ADD CONSTRAINT test_run_status_pkey PRIMARY KEY (test_run_id, node_identifier);
