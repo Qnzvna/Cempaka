362eeb2f0cdfefa823da673e0788067b32a53f54
Damian Serwin <damian.abel.serwin@gmail.com>
2021-01-27 17:00:12 +0100

    Fixes failing stalled test cleaner job

--- cyclone-daemon/src/main/java/org/cempaka/cyclone/listeners/RunningPayloadListener.java
+++ cyclone-daemon/src/main/java/org/cempaka/cyclone/listeners/RunningPayloadListener.java
@@ -12,6 +12,8 @@ import org.cempaka.cyclone.beans.MetricDataPoint;
 import org.cempaka.cyclone.core.channel.Payload;
 import org.cempaka.cyclone.core.channel.PayloadType;
 import org.cempaka.cyclone.core.channel.RunningPayload;
+import org.cempaka.cyclone.services.NodeIdentifierProvider;
+import org.cempaka.cyclone.storage.repositories.TestExecutionRepository;
 import org.cempaka.cyclone.storage.repositories.TestMetricRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,13 +24,19 @@ public class RunningPayloadListener implements BiConsumer<String, Payload>
     private static final Logger LOG = LoggerFactory.getLogger(RunningPayloadListener.class);
 
     private final TestMetricRepository testMetricRepository;
+    private final TestExecutionRepository testExecutionRepository;
+    private final NodeIdentifierProvider nodeIdentifierProvider;
     private final Clock clock;
 
     @Inject
     public RunningPayloadListener(final TestMetricRepository testMetricRepository,
+                                  final TestExecutionRepository testExecutionRepository,
+                                  final NodeIdentifierProvider nodeIdentifierProvider,
                                   final Clock clock)
     {
         this.testMetricRepository = checkNotNull(testMetricRepository);
+        this.testExecutionRepository = checkNotNull(testExecutionRepository);
+        this.nodeIdentifierProvider = checkNotNull(nodeIdentifierProvider);
         this.clock = checkNotNull(clock);
     }
 
@@ -36,11 +44,13 @@ public class RunningPayloadListener implements BiConsumer<String, Payload>
     public void accept(final String testExecutionId, final Payload payload)
     {
         if (payload.getType() == PayloadType.RUNNING) {
+            final UUID executionId = UUID.fromString(testExecutionId);
             LOG.debug("Running payload for {} {}.", testExecutionId, payload);
             final RunningPayload runningPayload = (RunningPayload) payload;
-            final long now = Instant.now(clock).toEpochMilli();
+            final Instant now = Instant.now(clock);
+            testExecutionRepository.updateTimestamp(executionId, nodeIdentifierProvider.get(), now);
             runningPayload.getMeasurements().forEach((name, value) ->
-                testMetricRepository.put(UUID.fromString(testExecutionId), new MetricDataPoint(now, name, value)));
+                testMetricRepository.put(executionId, new MetricDataPoint(now.toEpochMilli(), name, value)));
         }
     }
 }
